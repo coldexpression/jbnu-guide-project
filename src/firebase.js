0fbc2c083141803294fb18c43f17d3f8a31a7486
kimyb8870 <qhd0084@gmail.com>
2021-01-25 05:49:13 +0900

    Add: firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBDB2XrHdXwZ5OoAelGl7Er6TIAGIIxk2o",
+  authDomain: "jbnu-guide-project.firebaseapp.com",
+  projectId: "jbnu-guide-project",
+  storageBucket: "jbnu-guide-project.appspot.com",
+  messagingSenderId: "45195714163",
+  appId: "1:45195714163:web:f56e59336385a8bb30d406",
+  measurementId: "G-V01NVYNKMY",
+};
