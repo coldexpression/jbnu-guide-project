07e572e0a3b2119e5d104579a93d32778eb92f65
kimyb8870 <qhd0084@gmail.com>
2021-01-29 17:45:27 +0900

    Feature: google login button and continue button

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { authService, googleAuthProvider } from "../../firebase";
+import { Link } from "react-router-dom";
+import "./LoginPage.css";
 
 const LoginPage = () => {
+  const [user, setUser] = useState();
+
+  const loadGoogleUser = async () => {
+    await authService.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      }
+    });
+  };
+
+  const onSignInWithGoogle = () => {
+    authService
+      .signInWithPopup(googleAuthProvider)
+      .then((result) => {
+        // 나중에 필요할 때 사용
+        // const credential = result.credential;
+        // const token = credential.accessToken;
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log("!!Login Error!!");
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    loadGoogleUser();
+  }, []);
+
   return (
     <div>
-      <h1>LoginPage</h1>
+      <h1>Welcome</h1>
+      {user && <h2>{user.displayName}</h2>}
+      {user ? (
+        <Link to="/home">
+          <button>Continue</button>
+        </Link>
+      ) : (
+        <button onClick={onSignInWithGoogle}>Sign up with Google</button>
+      )}
     </div>
   );
 };
