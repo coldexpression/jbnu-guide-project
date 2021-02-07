fa3dd54fe4c25cc5f38f5553f57fcc01cccfef61
kimyb8870 <qhd0084@gmail.com>
2021-02-07 22:52:25 +0900

    Chore: temporary commit

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import { IconButton } from "@material-ui/core";
 import { authService } from "../../firebase";
+import "./ProfilePage.css";
+import { Link } from "react-router-dom";
 
 const ProfilePage = () => {
   // 유저가 스크랩해둔 자료들은 추후 리덕스를 통해 가져옴
@@ -18,37 +20,43 @@ const ProfilePage = () => {
     { id: 10, question: "Q. 테스트 질문사항", answer: "A. blahblah" },
   ]);
 
-  const [user, setUser] = useState();
+  const [user, setUser] = useState({ id: "kimyb" });
 
-  const loadCurrentUser = async () => {
-    await setUser(authService.currentUser);
-    await console.log(user);
-  };
+  // const loadCurrentUser = async () => {
+  //   await setUser(authService.currentUser);
+  //   await console.log(user);
+  // };
 
-  useEffect(() => {
-    loadCurrentUser();
-  }, []);
+  // useEffect(() => {
+  //   loadCurrentUser();
+  // }, []);
 
   return (
     <div className="profilepage">
       <header className="profilepage__header">
-        <IconButton>
-          <ArrowBackIcon />
-        </IconButton>
+        <Link to="/home">
+          <IconButton>
+            <ArrowBackIcon />
+          </IconButton>
+        </Link>
       </header>
-      <div className="profilepage__main">
+      <section className="profilepage__main">
         <div className="profilepage__userProfile">
-          <div className="profilepage__userinfo">
+          <div className="profilepage__userInfo">
+            <img
+              className="profilepage__userPic"
+              src="https://images.unsplash.com/photo-1600267185393-e158a98703de?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NTJ8fGF2YXRhcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            />
             <h4 className="profilepage__userName">username</h4>
             <div className="profilepage__userDetail">userdetail</div>
           </div>
-          <div>
+          <div className="profilepage__btnGroup">
             <button>Edit profile</button>
             <button>Log out</button>
           </div>
         </div>
-        <div className="profilepage__posts">
-          <ul>
+        <div className="profilepage__postlistWrapper">
+          <ul className="profilepage__postlist">
             {dummyPosts.map((post) => (
               <li>
                 <button>{post.question}</button>
@@ -57,7 +65,7 @@ const ProfilePage = () => {
             ))}
           </ul>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
